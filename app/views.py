79aa293fd65c26c2d6c0074b45323f7517cc7afc
kirbycm <kirbycm@gmail.com>
2019-07-22 10:07:19 -0400

    Default working directory to notebook root if not specified in request.

--- app/views.py
+++ app/views.py
@@ -12,11 +12,13 @@ from nbconvert.preprocessors import ExecutePreprocessor
 def index():
     data = request.get_json()
     # return request.data
+    print(request)
+    print(data)
     notebook_filename = data['notebook_filename']
     working_dir = data.get('working_directory')
 
     task = run_notebook.apply_async([notebook_filename, working_dir])
-    return jsonify({}), 202, {'Location': url_for('taskstatus', task_id=task.id)}
+    return jsonify({'state': 'PENDING',}), 202, {'Location': url_for('taskstatus', task_id=task.id)}
 
 
 @celery.task(bind=True)
@@ -29,7 +31,7 @@ def run_notebook(self, notebook_filename, working_dir):
         if working_dir:
             ep.preprocess(nb, {'metadata': {'path': working_dir}})
         else:
-            ep.preprocess(nb, {})
+            ep.preprocess(nb, {'metadata': {'path': config.NOTEBOOK_ROOT}})
 
     return {'status': 'Task finished'}
 
